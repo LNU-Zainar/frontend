975594b31af895b9922ddc09a95c8cb9ca291c86
simon <chillyond@gmail.com>
2021-12-12 10:21:49 +0800

    feat: 地图优化

--- src/pages/Map.vue
+++ src/pages/Map.vue
@@ -1,45 +1,111 @@
 <template>
-  <div class="map">
+  <div :class="['map', {'is-complete': isMapComplete}]">
     <div id="container"
       v-loading="isLoading"
       element-loading-text="地图加载中"
-      element-loading-background="#1a232c">
+      element-loading-background="transparent">
     </div>
+    <div class="left-bottom" v-if="isMapComplete">
+      <div class="line-item">
+        <el-button-group>
+          <el-button size="mini" icon="el-icon-position" @click="positionOrigin">
+            <!-- 原点 -->
+          </el-button>
+
+          <el-button size="mini" icon="el-icon-refresh" :loading="isRefreshLoading" @click="refreshData">
+            <!-- 刷新 -->
+          </el-button>
+
+          <!-- <el-button size="mini" :icon="showAllLocation ? 'el-icon-open' : 'el-icon-turn-off'" @click="showAllLocation=!showAllLocation">
+            {{ showAllLocation ? '隐藏空地点' : '显示空地点' }}
+          </el-button> -->
+        </el-button-group>
+      </div>
+
+      <div class="line-item">
+        显示全部地点
+        <el-switch
+          :disabled="isLoading"
+          v-model="showAllLocation"
+          active-color="#13ce66"
+          inactive-color="gray">
+        </el-switch>
+      </div>
+    </div>
+    
   </div>
 </template>
 
 <script>
 import mapAreaPath from '@/assets/mapAreaPath.json'
 import * as api from '@/common/api'
+import AMapLoader from '@amap/amap-jsapi-loader'
 /* global AMap */
 
 export default {
   data () {
+    const showAllLocation = localStorage.getItem('showAllLocation')
+
     return {
       isLoading: true,
-      locations: []
+      isRefreshLoading: false,
+      isMapComplete: false,
+      locations: [],
+      showAllLocation: showAllLocation === 'true'
     }
   },
   mounted () {
     this.map = null
     this.infoWindow = null
+    this.markers = []
 
     Promise.all([
-      this.loadSDK().then(this.initMap),
-      this.fetchData()
-    ]).then(() => {
-      this.addMarkers()
-    }).finally(() => {
+      this.initMap().then(this.onMapComplete).catch(() => {
+        this.$message.error('地图加载失败')
+      }),
+      this.fetchData(null, {
+        notifyType: ''
+      })
+      .catch(() => {
+        this.$message.error('数据获取失败')
+      })
+    ]).finally(() => {
       this.isLoading = false
     })
   },
   destroyed () {
-    this.script.remove()
     if (this.map) {
       this.map.destroy()
     }
   },
+  watch: {
+    showAllLocation (showAllLocation) {
+      localStorage.setItem('showAllLocation', showAllLocation)
+
+      this.markers.forEach(marker => {
+        if (!marker._originOpts.hasItems) {
+          if (showAllLocation) {
+            marker.show()
+          } else {
+            marker.hide()
+          }
+        }
+      })
+    },
+    locations () {
+      if (this.map) {
+        this.removeMarkers()
+        this.addMarkers()
+      }
+    }
+  },
   methods: {
+    refreshData () {
+      this.isRefreshLoading = true
+      this.fetchData().finally(() => {
+        this.isRefreshLoading = false
+      })
+    },
     fetchData () {
       return api.getMapLocation(null, {
         notifyType: 'f'
@@ -47,17 +113,21 @@ export default {
         this.locations = data
       })
     },
-    loadSDK () {
-      return new Promise((resolve, reject) => {
-        let script = document.createElement('script')
-        script.src = 'https://webapi.amap.com/maps?v=2.0&key=e765ee2c909344df70d61e4a6852adc5'
-        script.onload = resolve
-        script.onerror = reject
-        document.head.append(script)
-        this.script = script
-      })
-    },
-    initMap () {
+    async initMap () {
+      if (!window.AMap) {
+        await AMapLoader.load({
+          key: 'e765ee2c909344df70d61e4a6852adc5',
+          version: '2.0',
+          plugins: [],
+          // AMapUI: {
+          //   version: '1.1',
+          //   plugins: []
+          // },
+          // Loca: {
+          //   version: '2.0'
+          // },
+        })
+      }
       const bounds = [mapAreaPath.map(item => [item.R, item.Q])]
 
       const map = new AMap.Map('container', {
@@ -65,13 +135,16 @@ export default {
         resizeEnable: true,
         rotateEnable: true,
         pitchEnable: true,
-        showLabel: true,
         labelzIndex: 999,
-        rotation: 0,
         zoom: 16,
+        zooms: [14, 20],
         viewMode:'2D',
         pitch: 50,
-        center: [110.347924, 21.268779],
+        rotation: 0,
+        showLabel: true,
+        isHotspot: false,
+        features: ['bg','point','road','building'],
+        center: [110.347924, 21.268879],
         mapStyle: 'amap://styles/grey',
         layers: [
           new AMap.createDefaultLayer({
@@ -85,11 +158,6 @@ export default {
         ]
       })
 
-      const infoWindow = new AMap.InfoWindow({
-        position: location.position,
-        anchor: 'top-left'
-      })
-
       const area = new AMap.Polygon({
         zIndex: 1,
         path: bounds,
@@ -100,26 +168,39 @@ export default {
       })
       map.add(area)
 
-      this.infoWindow = infoWindow
+      return new Promise((resolve, reject) => {
+        map.on('complete', () => {
+          resolve(map)
+        })
+        map.on('error', reject)
+      })
+    },
+    async onMapComplete (map) {
       this.map = map
-    
-      // const outer = [
-      //   new AMap.LngLat(-360,90,true),
-      //   new AMap.LngLat(-360,-90,true),
-      //   new AMap.LngLat(360,-90,true),
-      //   new AMap.LngLat(360,90,true)
-      // ]
-      // const pathArray = [outer, ...bounds]
-      // const mask = new AMap.Polygon({
-      //   zIndex: 1,
-      //   pathL: pathArray,
-      //   strokeColor: '#0066ff',
-      //   strokeWeight: 1,
-      //   fillColor: '#71B3ff',
-      //   fillOpacity: .5
-      // })
-      // mask.setPath(pathArray)
-      // map.add(mask)
+      this.infoWindow = new AMap.InfoWindow({
+        anchor: 'top-left'
+      })
+      this.addMarkers()
+      this.positionOrigin()
+      return new Promise(resolve => {
+        setTimeout(() => {
+          this.isMapComplete = true
+          resolve()
+        })
+      })
+    },
+    positionOrigin () {
+      if (this.map) {
+        const immediately = this.isMapComplete
+          ? false : true
+        this.map.setFitView(null, immediately, [50, 50, 50, 50])
+      }
+    },
+    removeMarkers () {
+      this.markers.forEach(marker => {
+        marker.remove()
+      })
+      this.markers = []
     },
     addMarkers () {
       const infoWindow = this.infoWindow
@@ -145,9 +226,6 @@ export default {
       }
 
       this.locations.forEach(item => {
-        if (!item.items_total) {
-          return false
-        }
         const markerContent = `
         <div class="map-marker">
           <div class="${item.items_total ? 'map-marker-img' : 'map-marker-img is-empty'}">
@@ -178,10 +256,12 @@ export default {
         `
 
         const marker = new AMap.Marker({
+          visible: this.showAllLocation || Boolean(item.items_total),
           content: markerContent,
           anchor: 'bottom-left',
           infoContent,
           location: item,
+          hasItems: Boolean(item.items_total),
           position: item.position,
           offset: new AMap.Pixel(-12, 0)
         })
@@ -191,6 +271,7 @@ export default {
         marker.on('click', markerClick)
 
         map.add(marker)
+        this.markers.push(marker)
       })
     }
   }
@@ -198,12 +279,29 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-#container {
+.map {
   position: absolute;
-  background: #1a1a1a;
   left: 0;
   right: 0;
   top: 50px;
   bottom: 0;
+  max-width: 100%;
+  margin: 0;
+}
+#container {
+  width: 100%;
+  height: 100%;
+  background-color: #1a232c;
+}
+.left-bottom {
+  position: absolute;
+  bottom: 20px;
+  left: 20px;
+  font-size: 14px;
+  color: rgba(255,255,255, .8);
+  text-shadow: 1px 0 0 black, 0 1px 0 black, -1px 0 0 black,0 -1px 0 black;
+  .line-item {
+    margin-top: 10px;
+  }
 }
 </style>
